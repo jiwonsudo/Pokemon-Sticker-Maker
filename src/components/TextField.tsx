e7cc93c23b9b44ae03b6a42441be269d083ddc52
jiwonsudo <wondev112@gmail.com>
2024-03-05 23:14:11 +0900

    Feat: Create TextField Component

---
+++ src/components/TextField.tsx
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+const TextField = styled.input`
+  width: 150px;
+  padding-left: 8px;
+  font-size: 16px;
+  background-color: #f0f0f0;
+  font-family: Galmuri11;
+  clip-path: polygon(
+    0px calc(100% - 6px),
+    3px calc(100% - 6px),
+    3px calc(100% - 3px),
+    6px calc(100% - 3px),
+    6px 100%,
+    calc(100% - 6px) 100%,
+    calc(100% - 6px) calc(100% - 3px),
+    calc(100% - 3px) calc(100% - 3px),
+    calc(100% - 3px) calc(100% - 6px),
+    100% calc(100% - 6px),
+    100% 6px,
+    calc(100% - 3px) 6px,
+    calc(100% - 3px) 3px,
+    calc(100% - 6px) 3px,
+    calc(100% - 6px) 0px,
+    6px 0px,
+    6px 3px,
+    3px 3px,
+    3px 6px,
+    0px 6px
+  );
+`;
+
+export default TextField;
