0d425db0184ab7b80bbc2e448844dfc4784f1a26
jiwonsudo <wondev112@gmail.com>
2024-03-15 18:02:35 +0900

    Feat: Add Glowing effect on Seal

--- src/components/Content/Seal/index.tsx
+++ src/components/Content/Seal/index.tsx
@@ -16,6 +16,7 @@ const SealBackground = styled.div`
   width: 100%;
   height: 100%;
   background-color: #f6f3f3;
+  overflow: hidden;
 `;
 
 const SealIndexContainer = styled.div`
@@ -102,6 +103,25 @@ const SealCover = styled.div`
   height: 100%;
   position: absolute;
   top: 0; left: 0;
+  z-index: 4;
+`;
+
+const GlowEffectCover = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0; left: 0;
+  background: linear-gradient(
+    150deg,
+    transparent 20%,
+    #fefff1 30%,
+    #fff8e7 35%,
+    transparent 40%
+  );
+  filter: brightness(1) opacity(40%);
+  mix-blend-mode: color-dodge;
+  background-size: 500%;
+  background-position: 100%;
   z-index: 3;
 `;
 
@@ -123,11 +143,19 @@ const Seal = () => {
     const cursorPosY = event.nativeEvent.offsetY;
     const rotateX = 1/10 * cursorPosY - 20;
     const rotateY = -2/15 * cursorPosX + 20;
-    sealBg.current!.style.transform = `perspective(500px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+    const sealDiv: HTMLDivElement = sealBg.current!;
+    const glowDiv = sealBg.current!.children[3] as HTMLDivElement;
+    
+    sealDiv.style.transform = `perspective(500px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+    glowDiv.style.backgroundPosition = `${cursorPosX/5 + cursorPosY/5}%`;
+    
   }
 
   const resetSeal = () => {
-    sealBg.current!.style.transform = 'none';
+    const sealDiv: HTMLDivElement = sealBg.current!;
+    const glowDiv = sealBg.current!.children[3] as HTMLDivElement;
+    sealDiv.style.transform = 'none';
+    glowDiv.style.backgroundPosition = '100%';
   };
 
   const props = {
@@ -161,6 +189,7 @@ const SealView = ({ id, title, imgUri, sealBg, rotateSeal, resetSeal } : SealPro
             <CoverTextureImg></CoverTextureImg>
           </CreatorTitleContainer>
         </CreatorTitleSection>
+        <GlowEffectCover></GlowEffectCover>
       </SealBackground>
       <SealCover onMouseMove={ rotateSeal } onMouseLeave={ resetSeal }></SealCover>
     </SealContainer>
