7c6c4483bd23e8932e16e09df0929e116fea6a8e
jiwonsudo <wondev112@gmail.com>
2024-03-18 12:05:23 +0900

    Design: Add Download Button

--- src/components/Content/Controller/index.tsx
+++ src/components/Content/Controller/index.tsx
@@ -9,33 +9,47 @@ const ControlsContainer = styled.div`
   margin-top: 40px;
 `;
 
-const LineContainer = styled.div`
+const LineContainer = styled.div<{ $align?: string}>`
   width: 100%;
   margin: 5px 0;
   display: flex;
-  justify-content: space-between;
+  justify-content: ${ props => props.$align ? props.$align : 'space-between'};
   align-items: center;
 `;
 
-const Controller = () => {
-  return <ControllerView/>;
-};
+interface ControllerProps {
+  picBtnProps: { [key: string]: string },
+  genBtnProps: { [key: string]: string },
+  downBtnProps: { [key: string]: string },
+}
 
-const ControllerView = () => {
-  const viewProps = {
+const Controller = () => {
+
+  const props: ControllerProps = {
     picBtnProps: {
       $bgColor: '#54E50C',
       $color: '#323F2C',
       $activeBgColor: '#2e7908',
       $activeColor: '#171d14',
     },
-    getBtnProps: {
+    genBtnProps: {
       $bgColor: '#e5710c',
       $color: '#361b02',
       $activeBgColor: '#8b4507',
       $activeColor: '#090400',
-    }
+    },
+    downBtnProps: {
+      $bgColor: '#0c66e5',
+      $color: '#03214b',
+      $activeBgColor: '#063e8b',
+      $activeColor: '#01070f',
+    },
   };
+
+  return <ControllerView {...props} />;
+};
+
+const ControllerView = ({ picBtnProps, genBtnProps, downBtnProps } : ControllerProps) => {
   return (
     <ControlsContainer>
       <LineContainer>
@@ -47,8 +61,13 @@ const ControllerView = () => {
         <TextField></TextField>
       </LineContainer>
       <LineContainer>
-        <Button { ...viewProps.picBtnProps }>사진 업로드</Button>
-        <Button { ...viewProps.getBtnProps }>띠부씰 생성</Button>
+        <Button { ...picBtnProps }>사진 업로드</Button>
+        <Button { ...genBtnProps }>띠부씰 생성</Button>
+      </LineContainer>
+      <LineContainer $align="center">
+        <Button  { ...downBtnProps }>
+          띠부씰 저장하기
+        </Button>
       </LineContainer>
     </ControlsContainer>
   );
